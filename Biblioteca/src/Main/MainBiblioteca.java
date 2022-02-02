0daae3ec84654490ade856cdfa5ab0d14d8d0542
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-02-02 15:36:53 +0100

    comit prueba

--- Biblioteca/src/Main/MainBiblioteca.java
+++ Biblioteca/src/Main/MainBiblioteca.java
@@ -5,6 +5,7 @@ public class MainBiblioteca {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Biblioteca");
+		System.out.println("Por Luis Álvarez Cueva");
 	}
 
 }
